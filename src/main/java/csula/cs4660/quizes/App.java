a66748e3041c977c74d6f299a2dca20585fec7ab
mohanmb91 <mohankumarmbuniv@gmail.com>
2016-10-16 19:59:34 -0700

    quiz2

--- src/main/java/csula/cs4660/quizes/App.java
+++ src/main/java/csula/cs4660/quizes/App.java
@@ -1,5 +1,8 @@
 package csula.cs4660.quizes;
 
+import csula.cs4660.graphs.Edge;
+import csula.cs4660.graphs.Node;
+import csula.cs4660.quizes.models.DTO;
 import csula.cs4660.quizes.models.State;
 
 import java.util.*;
@@ -8,48 +11,105 @@ import java.util.*;
  * Here is your quiz entry point and your app
  */
 public class App {
+	static boolean isGoalReached = false;
     public static void main(String[] args) {
         // to get a state, you can simply call `Client.getState with the id`
         State initialState = Client.getState("10a5461773e8fd60940a56d2e9ef7bf4").get();
-        System.out.println(initialState);
-        // to get an edge between state to its neighbor, you can call stateTransition
-        System.out.println(Client.stateTransition(initialState.getId(), initialState.getNeighbors()[0].getId()));
+        State goal = Client.getState("e577aa79473673f6158cc73e0e5dc122").get();
+        BFS(initialState); //BFS implementation
+        System.out.println();
+        System.out.println("Dijkstra's"); 
+        System.out.println();
+        Dijikstra(initialState,goal);
+    }
+
+    private static void Dijikstra(State initialState, State goal) {
+    	PriorityQueueQuiz frontier = new PriorityQueueQuiz();
+        Set<State> exploredSet = new HashSet<>();
+        Map<State, State> parents = new HashMap<>();
+    	
+    	frontier.enqueue(initialState, 0);
+    	exploredSet.add(initialState);
+    	
+    	while(!frontier.isEmpty()){
+    	State firstNodeOfPriorityQueue = frontier.dequeue();
+    	exploredSet.add(firstNodeOfPriorityQueue);
+    	for(State eachNeibhour : Client.getState(firstNodeOfPriorityQueue.getId()).get().getNeighbors()){
+			if(!exploredSet.contains(eachNeibhour)){
+				Optional<DTO> stateTranscition = Client.stateTransition(firstNodeOfPriorityQueue.getId(), eachNeibhour.getId());
+				int neibhourWeight = stateTranscition.get().getEvent().getEffect();
+				int currentNodeWeight = frontier.getPriotiyOfState(firstNodeOfPriorityQueue);
+				int combinedWeight = neibhourWeight + currentNodeWeight;
+				if(frontier.getPriotiyOfState(eachNeibhour) == -1){
+					frontier.enqueue(eachNeibhour, combinedWeight);
+					parents.put(eachNeibhour,firstNodeOfPriorityQueue);
+				}else{
+					if(combinedWeight > frontier.getPriotiyOfState(eachNeibhour)){
+						frontier.remove(eachNeibhour);
+						frontier.enqueue(eachNeibhour, combinedWeight);	
+						parents.put(eachNeibhour, firstNodeOfPriorityQueue);
+					}
+				}
+			}
+		}
+    	}
+    	findDepth(parents,goal,initialState);
+        
+		
+	}
+
+	private static void BFS(State initialState) {
 
         Queue<State> frontier = new LinkedList<>();
         Set<State> exploredSet = new HashSet<>();
         Map<State, State> parents = new HashMap<>();
         frontier.add(initialState);
-
+        System.out.println("BFS Path:");
+        System.out.println();
         while (!frontier.isEmpty()) {
             State current = frontier.poll();
             exploredSet.add(current);
-
+            if (current.getId().equals("e577aa79473673f6158cc73e0e5dc122")) {
+                // construct actions from endTile
+            	//parents.put(neighbor, current);
+                findDepth(parents, current, initialState);
+                isGoalReached = true;
+                
+            }
             // for every possible action
             for (State neighbor: Client.getState(current.getId()).get().getNeighbors()) {
-                // state transition
-                if (neighbor.getId().equals("e577aa79473673f6158cc73e0e5dc122")) {
-                    // construct actions from endTile
-                    System.out.println("found solution with depth of " + findDepth(parents, current, initialState));
-                }
+            	if(isGoalReached)
+            		break;
                 if (!exploredSet.contains(neighbor)) {
-                    parents.put(neighbor, current);
-                    frontier.add(neighbor);
-                }
+                     parents.put(neighbor, current);
+                     frontier.add(neighbor);
+                 }
             }
+            if(isGoalReached)
+        		break;
         }
+        if(!isGoalReached)
+        	System.out.println("Not found solution");
+        if(isGoalReached)
+        	isGoalReached = true;
+       
+		
+	}
 
-        System.out.println("Not found solution");
-    }
-
-    public static int findDepth(Map<State, State> parents, State current, State start) {
+	public static void findDepth(Map<State, State> parents, State current, State start) {
         State c = current;
+        Stack<String> stack = new Stack<String>();
         int depth = 0;
-
+        Optional<DTO> stateTranscition;
         while (!c.equals(start)) {
+        	stateTranscition = Client.stateTransition(parents.get(c).getId(),c.getId() );
+        	stack.push(parents.get(c).getLocation().getName() +" : "+c.getLocation().getName() + " : " +stateTranscition.get().getEvent().getEffect() );
             depth ++;
             c = parents.get(c);
         }
-
-        return depth;
+        while(!stack.empty()){
+        	System.out.println(stack.pop());
+        }
+        
     }
 }
