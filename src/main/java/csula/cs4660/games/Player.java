8e46d6186d5a6e697cf697a92f75565d6a7282ca
mohanmb91 <mohankumarmbuniv@gmail.com>
2016-12-10 09:05:31 -0800

    test cases passed for HW4

--- src/main/java/csula/cs4660/games/Player.java
+++ src/main/java/csula/cs4660/games/Player.java
@@ -591,14 +591,7 @@ class Player {
 
         TreeMap<String, String> possibleMoves = new TreeMap<String, String>();
         NodeTron<MiniMaxStateTron> lastNode = start;
-//        int level;
-//        if(N > 3){
-//        level = 1;
-//        radius = 15; pull request change
-//        }else{
-//        	level = 2;
-//        	radius = 100;
-//        }
+
     	while(level > 0 && !frontier.isEmpty()){
     		NodeTron<MiniMaxStateTron> currentNode = frontier.poll();
     		if(currentNode.equals(lastNode)){
